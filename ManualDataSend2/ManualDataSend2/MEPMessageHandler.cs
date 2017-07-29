a990e0bf39a0bb7a1188818c251c161df1f41ab2
bar-ang <bar.ang16@gmail.com>
2017-07-30 02:12:09 +0300

    added class for MEP

---
+++ ManualDataSend2/ManualDataSend2/MEPMessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManualDataSend2
+{
+    class MEPMessageHandler
+    {
+        public static async void sendMEP(MEP message)
+        {
+            await AzureIoTHub.SendDeviceToCloudMessageAsync(message.ToString());
+        }
+        public static async Task<MEP> receiveMEP()
+        {
+            string res = await AzureIoTHub.ReceiveCloudToDeviceMessageAsync();
+            return MEP.parseMEP(res);
+        }
+    }
+}
