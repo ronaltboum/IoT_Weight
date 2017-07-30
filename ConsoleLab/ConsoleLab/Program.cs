91a287cd4c593718afa6e052c3530ee5785eff64
bar-ang <bar.ang16@gmail.com>
2017-07-30 18:49:38 +0300

    Created class for parsing MEP

---
+++ ConsoleLab/ConsoleLab/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint ip = 0x11111111;
+            long mac = 0x123456789ABC;
+
+            MEP mep = new MEP(MEPDevType.RBPI, mac, ip, MEPCallbackAction.Response);
+            Console.WriteLine(mep.DevType);
+            Console.WriteLine(mep.MacAddr);
+            Console.WriteLine(mep.IpAddr);
+            Console.WriteLine(mep.CallbackAction);
+            Console.WriteLine(mep.Date);
+            mep.timeStamp();
+            Console.WriteLine(mep.Date);
+            Console.WriteLine();
+            string mepstr = mep.ToString();
+            Console.WriteLine(mepstr);
+
+            MEP mep2 = MEP.deserializeMEP(mepstr);
+            Console.WriteLine(mep2.MacAddr);
+            Console.WriteLine(mep2.IpAddr);
+
+            Console.ReadLine();
+        }
+    }
+}
--- ManualDataSend2/ManualDataSend2/MEPMessageHandler.cs
+++ ManualDataSend2/ManualDataSend2/MEPMessageHandler.cs
@@ -6,16 +6,69 @@ using System.Threading.Tasks;
 
 namespace ManualDataSend2
 {
+    /**
+     * Handling sending and receiving MEP Messages
+     */
     class MEPMessageHandler
     {
+        /**
+         * Send MEP message
+         */
         public static async void sendMEP(MEP message)
         {
+            message.timeStamp();
             await AzureIoTHub.SendDeviceToCloudMessageAsync(message.ToString());
         }
+        /**
+         * Receive MEP Message
+         * NOTE: This method does not send response!
+         */
         public static async Task<MEP> receiveMEP()
         {
             string res = await AzureIoTHub.ReceiveCloudToDeviceMessageAsync();
-            return MEP.parseMEP(res);
+            return MEP.deserializeMEP(res);
+        }
+        /**
+         * Send MEP message and wait for response.
+         * NOTE: This method does not send response!
+         * If the message's callback action is NOP, 'null' is returned.
+         */
+        public static async Task<MEP> sendReceive(MEP message)
+        {
+            sendMEP(message);
+            if (message.CallbackAction == MEPCallbackAction.NoOperation)
+                return null;
+            return await receiveMEP();
+        }
+        /**
+        * Listen to incoming messages and response them.
+        */
+        public static async Task<MEP> listen(long mac, uint ip)
+        {
+            MEP income = await receiveMEP();
+            MEP response = new MEP(MEPDevType.RBPI, mac, ip, MEPCallbackAction.NoOperation);
+
+            MEPCallbackAction callback = income.CallbackAction;
+
+            switch (callback)
+            {
+                case MEPCallbackAction.Response:
+                    response.CallbackAction = MEPCallbackAction.Acknowledge;
+                    sendMEP(response);
+                    break;
+                case MEPCallbackAction.Acknowledge:
+                    response.CallbackAction = MEPCallbackAction.NoOperation;
+                    response.IpAddr = 0;
+                    response.MacAddr = 0;
+                    sendMEP(response);
+                    break;
+
+                case MEPCallbackAction.NoOperation:
+                    break;
+
+            }
+
+            return income;
         }
     }
 }
