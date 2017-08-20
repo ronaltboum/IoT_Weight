415578b83e76ec9e8b4d2315484c897b44c37909
RamyLerner <san85@walla.com>
2017-08-20 16:46:15 +0300

    Add files via upload

--- MEPClient.cs
+++ MEPClient.cs
@@ -4,49 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using Microsoft.WindowsAzure.MobileServices;
+using weighJune28;
 
 namespace ManualDataSend2
 {
-	class MEPClient{
-		
-		private uint myIp;
+    class MEPClient
+    {
+
+        private uint myIp;
         private long myMac;
         private long raspberryMAC;
-		
-		//list of nearby Raspberry IPs
-		private LinkedList<long> nearbyRasps;
-		
-		public uint MyIp { get => myIp; set => myIp = value; }
+
+        //list of nearby Raspberry IPs
+        private List<long> nearbyRasps;
+
+        public MEPClient(uint myIp, long myMac, List<long> nearbyRasps)
+        {
+            this.myIp = myIp;
+            this.myMac = myMac;
+            this.nearbyRasps = new List<long>(nearbyRasps);
+        }
+
+        public uint MyIp { get => myIp; set => myIp = value; }
         public long MyMac { get => myMac; set => myMac = value; }
-		public long RaspberryMAC { get => raspberryMAC; set => raspberryMAC = value; }
-		
-		private IMobileServiceTable<RaspberryTable> RaspberryTableRef;
-		
-		
-		
-		
-		
-		private LinkedList<long> getNearbyRasps{
-			
-		
-       
-            
-            
-        // Create a new instance field for this activity.
-		}
-		
-		//sends a MEP to all nearby Raspberries 
-		private async void sendMepsToNearbyRasps(){
-			foreach (long rasp in nearbyRasps){
-				MEP MEPmessage = new MEP (MEPDevType.APP, MyMac, MyIp, rasp, MEPCallbackAction.Response);
-				sendMEP(MEPmessage);
-			}
-			
-		}
-		
-		
-		
-		/**
+        public long RaspberryMAC { get => raspberryMAC; set => raspberryMAC = value; }
+        public List<long> NearbyRasps { get => new List<long>(nearbyRasps); }
+
+
+        //sends a MEP to all nearby Raspberries 
+        private void sendMepsToNearbyRasps()
+        {
+            foreach (long rasp in nearbyRasps)
+            {
+                MEP MEPmessage = new MEP(MEPDevType.APP, MyMac, MyIp, rasp, MEPCallbackAction.Response);
+                sendMEP(MEPmessage);
+            }
+
+        }
+
+        private const int TIMEOUT = 5000;
+
+        public void run()
+        {
+            sendMepsToNearbyRasps();
+
+            bool waitingForMessages = true;
+            while (waitingForMessages)
+            {
+                Task<MEP> mep_task = receiveMEP();
+                if (!mep_task.Wait(TIMEOUT))
+                {
+                    Debug.WriteLine("No more messaged received");
+                    waitingForMessages = false;
+                    break;
+                }
+                MEP msg = mep_task.Result;
+                raspberryMAC = msg.MacAddr;
+
+                if(msg.CallbackAction == MEPCallbackAction.Acknowledge)
+                {
+                    MEP response = new MEP(MEPDevType.APP, myMac, myIp, raspberryMAC, MEPCallbackAction.NoOperation);
+                    sendMEP(response);
+                }
+
+            }
+        }
+
+
+
+        /**
          * Receive MEP messages from the cloud
          */
         private async Task<MEP> receiveMEP()
@@ -64,18 +91,6 @@ namespace ManualDataSend2
             message.timeStamp();
             await AzureIoTHub.SendDeviceToCloudMessageAsync(message.ToString());
         }
+    }
 
-
-
-		
-		
-		
-		
-	}
-	
 }
-
-		
-		
-		
-		
